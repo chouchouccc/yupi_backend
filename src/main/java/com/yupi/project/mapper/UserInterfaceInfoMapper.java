75250c146bf743eb4f65e2006a479ca3847dd44b
shangyingzhi <shangyz89@163.com>
2023-03-23 17:05:01 +0800

    api后端项目代码

---
+++ src/main/java/com/yupi/project/mapper/UserInterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.yupi.project.mapper;
+
+import com.yupi.project.model.entity.UserInterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 86152
+* @description 针对表【user_interface_info(用户调用接口关系)】的数据库操作Mapper
+* @createDate 2023-03-18 16:01:06
+* @Entity com.yupi.project.model.entity.UserInterfaceInfo
+*/
+public interface UserInterfaceInfoMapper extends BaseMapper<UserInterfaceInfo> {
+
+}
+
+
+
+
